5ef351b02a70cec05a8a664887d313c866d25231
user <user@users-mbp.hsd1.nj.comcast.net>
2019-05-11 23:03:23 -0400

    Addedapp

---
+++ src/main/java/edu/drexel/SpringDataSakilaApp/SpringDataSakilaApp.java
@@ -0,0 +1,15 @@
+package edu.drexel.SpringDataSakilaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSakilaApp {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringDataSakilaApp.class, args);
+		
+	}
+	
+}
